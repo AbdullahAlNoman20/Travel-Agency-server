f1e946f6fab889d8629501b4586ef5b80e283ce4
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2024-10-09 18:38:38 +0600

    Post Packages fron frontend to DB, Authentication.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000;
 const app = express()
 
 
+require('dotenv').config();
+
+
 // Middleware
 app.use(cors())
 app.use(express.json())
@@ -14,11 +17,13 @@ app.use(express.json())
 // Create Connection with the Database
 const db = mysql.createConnection({
     host: "localhost",
-    user: "noman",
-    password: "12345",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "project01"
 })
 
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
 
 // Check the DB Perfectly connect or not
 db.connect((error)=>{
@@ -54,12 +59,34 @@ app.get("/register_users",(req,res)=>{
 // Post Data In DB
 app.post('/register_users', async(req,res)=>{
 
+    const newUser = req.body;
         const userName =  req.body.userName
-        // const number = req.body.number
         const email = req.body.email
         const password = req.body.password
+    // console.log(newUser)
 
       db.query ("INSERT INTO register_users (userName,email,password) VALUES(?,?,?)",[userName,email,password]),
+        (err,result)=>{
+            if(result){
+                res.send(result)
+            }
+            else{
+                res.send(err)
+            }
+        }
+})
+
+
+// Post Data In DB
+app.post('/package', async(req,res)=>{
+
+    const newPackage = req.body
+        const placeName =  req.body.placeName
+        const description = req.body.description
+        
+    // console.log(newPackage)
+
+      db.query ("INSERT INTO package (placeName,description) VALUES(?,?)",[placeName,description]),
         (err,result)=>{
             if(result){
                 res.send(result)
